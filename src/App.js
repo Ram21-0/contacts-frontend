791ede896a482cf99c6484d0deafc9ced3370c71
Digvijay <digishek@gmail.com>
2022-01-18 17:10:45 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import { Link, BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router ,Switch ,Route} from "react-router-dom"
 import Header from "./components/Header";
 
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component ={LandingPage}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/register" component={Register}></Route>
+          <Route path="/cts" component ={Contact}></Route>
+          <Route path="/myProfile" component={MyProflie}></Route>
+        </Switch>
+      </div>
       <Header/>
       <Routes>
+
         <Route path="/" element={<Link to="/contacts">Link</Link>}>  </Route>
         <Route path="/contacts"/>
       </Routes>
-  </BrowserRouter>
+  </Router>
   );
 }
 
